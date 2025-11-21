99d764fece88666ddd715c822a80fb97c4d25d15
prubioamzubiri <prubioam@zubirimanteo.com>
2025-11-21 08:44:58 +0100

    Proyecto iniciado

---
+++ Assets/Settings/NewMonoBehaviourScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("NewMonoBehaviourScript has started.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
